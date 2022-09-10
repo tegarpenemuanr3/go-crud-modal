85db7bfb7052c25166c635261ad347e8aa151bfd
tegarpenemuanr3 <tegarpenemuan@gmail.com>
2022-09-10 23:46:11 +0700

    first commit

---
+++ db/go_crud_modal.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 10 Sep 2022 pada 18.43
+-- Versi server: 10.4.24-MariaDB
+-- Versi PHP: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `go_crud_modal`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nama_lengkap` varchar(300) NOT NULL,
+  `jenis_kelamin` enum('L','P') NOT NULL,
+  `tempat_lahir` varchar(300) NOT NULL,
+  `tanggal_lahir` date NOT NULL,
+  `alamat` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/jeypc/go-crud-modal/controllers/mahasiswacontroller"
+)
+
+func main() {
+	http.HandleFunc("/", mahasiswacontroller.Index)
+	http.HandleFunc("/mahasiswa/get_form", mahasiswacontroller.GetForm)
+	http.HandleFunc("/mahasiswa/store", mahasiswacontroller.Store)
+	http.HandleFunc("/mahasiswa/delete", mahasiswacontroller.Delete)
+
+	http.ListenAndServe(":8000", nil)
+}
+
+// go run main.go
+// localhost:8000
